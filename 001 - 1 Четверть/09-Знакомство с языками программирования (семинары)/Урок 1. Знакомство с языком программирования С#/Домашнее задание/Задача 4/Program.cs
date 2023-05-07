32931d2cb6a60be20ae7d9ff23b88bc19f8dfe2b
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-07 20:33:39 +0300

    added screenshot to Task2

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 1. Знакомство с языком программирования С#/Домашнее задание/Задача 4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int x3 = Convert.ToInt32(Console.ReadLine());
+
+int max = x1;
+
+if(x1 > max) {
+    max = x1; 
+} 
+if(x2 > max) {
+    max = x2; 
+} 
+if(x3 > max) {
+    max = x3; 
+}
+
+Console.WriteLine("Максимальное значение трёх чисел: " + max);
