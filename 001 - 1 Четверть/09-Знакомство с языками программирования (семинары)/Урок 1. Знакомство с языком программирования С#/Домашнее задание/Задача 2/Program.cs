ffc3909d563d472b89ce2a1cf732e04e406d412c
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-07 19:34:14 +0300

    added Task 2

--- 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 1. Знакомство с языком программирования С#/Домашнее задание/Задача 2/Program.cs
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 1. Знакомство с языком программирования С#/Домашнее задание/Задача 2/Program.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net; //Include this namespace
+﻿Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
 
-
-namespace IpProto
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string hostName = Dns.GetHostName(); // Retrive the Name of HOST
-            Console.WriteLine(hostName);
-           // Get the IP
-            string myIP = Dns.GetHostByName(hostName).AddressList[1].ToString();
-            Console.WriteLine("My IP Address is :"+myIP);
-            Console.ReadKey();
-        }
-    }
+if(a > b) {
+    Console.WriteLine("max = " + a);
+} 
+if (b > a) {
+    Console.WriteLine("max = " + b);
+}
+if(b == a) {
+    Console.WriteLine("Оба значения равны!!!");
 }
