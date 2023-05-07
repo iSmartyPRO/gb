06aab2b6e40aa2d20a388f7eaed1975b22c8e1af
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-07 22:01:08 +0300

    added Task7-HARD

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 1. Знакомство с языком программирования С#/Домашнее задание/Задача 7/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if(x > 10) {
+    while(x > 99) x /= 10;
+    Console.WriteLine(x % 10);
+} else {
+    Console.WriteLine("Нет");
+}
