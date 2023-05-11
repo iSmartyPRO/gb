26bfbbffa7554110a036ff5f01b7ed2f8c32c252
Ilias Aidar <Ilias.Aidar@gescons.ru>
2023-05-11 20:26:33 +0300

    added task13

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 2. Базовые алгоритмы/Домашние задания/Задача 13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число более трёх знаков: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 99) {
+    while(num > 1000) num /= 10;
+    Console.WriteLine(num % 10);
+} else {
+    Console.WriteLine("третьей цифры нет");
+}
