d19ae468edf814758c416d2437a6378a24de0fe4
Ilias Aidar <Ilias.Aidar@gescons.ru>
2023-05-11 20:03:27 +0300

    added task10

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 2. Базовые алгоритмы/Домашние задания/Задача 10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 99 && num < 1000) {
+    int num2 = (num % 100 - num % 10) / 10;
+    Console.WriteLine("Вторая цифра числа " + num + " является " + num2);
+
+} else {
+    Console.WriteLine("Ошибка! На вход принимается только трёхзначные числа");
+}
