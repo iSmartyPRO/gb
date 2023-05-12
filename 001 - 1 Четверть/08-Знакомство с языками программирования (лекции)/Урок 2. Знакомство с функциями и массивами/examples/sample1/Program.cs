9c3ab54ba176b08da1dbb0d526c343dc1acf9c78
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-12 16:57:24 +0300

    added lesson2

---
+++ 001 - 1 Четверть/08-Знакомство с языками программирования (лекции)/Урок 2. Знакомство с функциями и массивами/examples/sample1/Program.cs
@@ -0,0 +1,11 @@
+﻿// f(x) = x^2 + 1
+
+double f(double x) {
+    double result = x * x + 1;
+    return result;
+}
+
+System.Console.WriteLine(f(2));
+System.Console.WriteLine(f(5));
+System.Console.WriteLine(f(10));
+System.Console.WriteLine(f(16));
