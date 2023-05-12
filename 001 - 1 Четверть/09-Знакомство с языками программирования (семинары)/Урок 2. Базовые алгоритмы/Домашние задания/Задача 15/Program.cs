876dcd01993f9b4555c0c27aef2816043043be07
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-12 11:27:10 +0300

    added task15

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 2. Базовые алгоритмы/Домашние задания/Задача 15/Program.cs
@@ -0,0 +1,27 @@
+﻿int Prompt(string message) {
+    System.Console.Write(message);
+    int result = Convert.ToInt32(System.Console.ReadLine());
+    return result;
+}
+
+bool isWeekNumber(int num){
+    if (num > 0 && num <=7) {
+        return true;
+    }
+    System.Console.WriteLine("Это не день недели");
+    return false;
+}
+
+bool isWeekEnd(int weekDay) {
+    return (weekDay == 6 || weekDay == 7) ? true : false;
+}
+
+int weekDay = Prompt("Введите день недели: ");
+if(isWeekNumber(weekDay)) {
+    System.Console.WriteLine("Проверка выходного дня:");
+    if(isWeekEnd(weekDay)) {
+        System.Console.WriteLine("Да, сегодня выходной!!!");
+    } else {
+        System.Console.WriteLine("Нет, сегодня работаем!");
+    }
+}
