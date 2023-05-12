b6e66a018203d3e51bac2b9eb95d24564dc7d78d
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-12 18:05:12 +0300

    added task3 with switch/case construction

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 1. Знакомство с языком программирования С#/examples/example04/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1:
+        Console.Write("Понедельник");
+        break;
+    case 2:
+        Console.Write("Вторник");
+        break;
+    case 3:
+        Console.Write("Среда");
+        break;
+    case 4:
+        Console.Write("Четверг");
+        break;
+    case 5:
+        Console.Write("Пятница");
+        break;
+    case 6:
+        Console.Write("Суббота");
+        break;
+    case 7:
+        Console.Write("Воскресенье");
+        break;
+    default:
+        Console.Write("Ошибка!!! Введённая цифра не является днём недели!");
+        break;
+
+}
