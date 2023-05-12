2bd4ccc66a59c58346cdd61c8eb6f45f52ebe8ce
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-12 11:12:15 +0300

    added task13

--- 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 2. Базовые алгоритмы/Домашние задания/Задача 13/Program.cs
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 2. Базовые алгоритмы/Домашние задания/Задача 13/Program.cs
@@ -1,9 +1,18 @@
-﻿Console.Write("Введите число более трёх знаков: ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-if(num > 99) {
+﻿int Prompt(string message) {
+    System.Console.Write(message);
+    int result = Convert.ToInt32(System.Console.ReadLine());
+    return result;
+}
+bool Check(int num) {
+    return (num < 100) ? false : true;
+}
+int GetThirdNum(int num) {
     while(num > 1000) num /= 10;
-    Console.WriteLine(num % 10);
+    return num % 10;
+}
+int num = Prompt("Введите трёхзначное число: ");
+if(Check(num)) {
+    System.Console.WriteLine($"Третья цифра числа {num} является {GetThirdNum(num)}");
 } else {
-    Console.WriteLine("третьей цифры нет");
+    System.Console.WriteLine("третьей цифры нет");
 }
