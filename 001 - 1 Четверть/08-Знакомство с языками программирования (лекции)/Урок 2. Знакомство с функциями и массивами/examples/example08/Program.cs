d26ab149b872620ab274a353ae09bd28459f5cb6
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-12 16:05:07 +0300

    added example08 with function one lined

--- 001 - 1 Четверть/08-Знакомство с языками программирования (лекции)/Урок 2. Знакомство с функциями и массивами/examples/example08/Program.cs
+++ 001 - 1 Четверть/08-Знакомство с языками программирования (лекции)/Урок 2. Знакомство с функциями и массивами/examples/example08/Program.cs
@@ -15,10 +15,12 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 313;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 // int max = a1;
 // if(b1 > max) max = b1;
