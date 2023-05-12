9236127723d1636b32cb4b60483754d89a6c9d6b
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-12 18:25:28 +0300

    added task5 to lesson1

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 1. Знакомство с языком программирования С#/examples/task5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int start = num - num - num;
+
+while(start <= num ) {
+    Console.Write($"{start}, ");
+    start++;
+}
