444e5297745467f1d402e9b0d144a6699413ff63
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-12 16:29:17 +0300

    added example10 into lesson2

---
+++ 001 - 1 Четверть/08-Знакомство с языками программирования (лекции)/Урок 2. Знакомство с функциями и массивами/examples/example10/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] arr = { 21, 42, 35, 42, 54, 16, 37, 58, 12 };
+
+int n = arr.Length;
+int find = 16;
+int index = 0;
+
+/* for(int i = 0; i < n; i++) {
+    if(arr[i] == find) {
+        System.Console.WriteLine($"Found. Index is: {i}");
+        return;
+    }
+} */
+
+while (index < n)
+{
+    if (arr[index] == find) {
+        System.Console.WriteLine($"Found. Index is: {index}");
+        break;
+    }
+        index++;
+}
