6dc328dad2e88b7da2c6056610abf7265c429e9a
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-12 18:32:12 +0300

    added task7 to lesson1

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 1. Знакомство с языком программирования С#/examples/task7/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 99 && num < 1000) {
+    Console.WriteLine(num % 10);
+} else {
+    Console.WriteLine("Ошибка!!! Вы ввели не правильное число. Нужно трёхзначное");
+}
