0618ddf28a3a8defb21044c216f53834954af459
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-24 20:30:30 +0300

    added task27

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 4. Функции/Домашние задания/Задача 27/Program.cs
@@ -0,0 +1,20 @@
+﻿int Prompt(string message) {
+    System.Console.Write(message);
+    int result = Convert.ToInt32(System.Console.ReadLine());
+    return result;
+}
+
+int sumAll(int num)
+{
+    int res = 0;
+    while (num > 0)
+    {
+        res += num % 10;
+        num = num / 10;
+    }
+    return res;
+}
+
+int num = Prompt("Введите число: ");
+
+Console.WriteLine($"Сумма чисел {num} равен {sumAll(num)}");
