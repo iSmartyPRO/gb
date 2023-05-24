5a289ae150e9175665d96881a1eb1a09bf0fb6b8
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2023-05-24 20:19:52 +0300

    added task 25

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 4. Функции/Домашние задания/Задача 25/Program.cs
@@ -0,0 +1,20 @@
+﻿int Prompt(string message) {
+    System.Console.Write(message);
+    int result = Convert.ToInt32(System.Console.ReadLine());
+    return result;
+}
+
+int funcStepen(int baseNum, int stepenNum)
+{
+    int num = 1;
+    for (int i = 0; i < stepenNum; i++)
+    {
+        num *= baseNum;
+    }
+    return num;
+}
+
+int baseNum = Prompt("Введите основание: ");
+int stepenNum = Prompt("Введите степень: ");
+
+Console.WriteLine(funcStepen(baseNum, stepenNum));
