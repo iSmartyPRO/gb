5ea47d74178cca77174d45292a85f193a3cb05ca
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2024-02-21 23:36:51 +0300

    added lesson1:task5

---
+++ 001 - 1 Четверть/08-Знакомство с языками программирования (лекции)/Урок 1. Введение/lesson/task5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter value: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+Console.WriteLine("Result is following:");
+while(i <= N) {
+    // Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write(' ');
+    i++;
+}
