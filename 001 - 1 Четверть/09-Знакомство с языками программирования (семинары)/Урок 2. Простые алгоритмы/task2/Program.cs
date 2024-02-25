b5113def0bf6256e815c3f97ef398225804317d2
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2024-02-25 16:29:35 +0300

    added lesson2 task2

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 2. Простые алгоритмы/task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает 
+// на вход трёхзначное число и возводит
+// вторую цифру этого числа в степень
+// равную третьей цифре
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num2 = number / 10 % 10;
+int num3 = number % 10;
+
+Console.WriteLine($"Результат: {Math.Pow(num2, num3)}");
