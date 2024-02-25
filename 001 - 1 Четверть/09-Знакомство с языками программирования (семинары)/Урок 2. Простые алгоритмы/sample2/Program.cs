6dcf66c4ca1080e95c2c033bf403c1b527048d26
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2024-02-25 16:16:54 +0300

    added lesson2 sample2

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 2. Простые алгоритмы/sample2/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = new Random().Next(50,200);
+
+string numberString = Convert.ToString(number);
+Console.WriteLine($"Строковое число на входе: {numberString}");
+
+if (numberString.Length == 3)
+{
+    int num1 = Convert.ToInt32(Convert.ToString(numberString[0]));
+    int num2 = Convert.ToInt32(Convert.ToString(numberString[2]));
+
+    Console.WriteLine("--- Вариант 1 ---");
+    Console.Write(num1);
+    Console.WriteLine(num2);
+
+    Console.WriteLine("--- Вариант 2 ---");
+    Console.WriteLine($"{num1}{num2}");
+
+    Console.WriteLine("--- Вариант 3 ---");
+    Console.WriteLine($"{num1} {num2}");
+} else {
+    Console.WriteLine("Введено не трёхначное строковое число");
+}
