f34e4928e5f3a883bec0ab2ac1c158ec3befe4d3
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2024-02-26 20:36:53 +0300

    added lesson3 task2

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 3. Массивы/task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задание 2
+// Задайте массив из 10 элементов, заполненный числами из
+// промежутка [-10, 10]. Замените отрицательные элементы на
+// положительные, а положительные на отрицательные.
+// Пример
+// [1 -5 6]
+// => [-1 5 -6]
+
+int[] arr = new int[10];
+
+Console.WriteLine("Заполняем массив данными");
+for(var i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(-10, 10);
+}
+
+Console.WriteLine("Отображаем массив с заполненными данными");
+foreach(int item in arr)
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine("\n");
+
+Console.WriteLine("Инвертируем заполненные числа в массиве");
+for(var i = 0; i < arr.Length; i++)
+{
+    if(arr[i] > 0) arr[i] = -arr[i];
+        else arr[i] *= -1;
+}
+
+Console.WriteLine("Отображаем массив с заполненными данными");
+foreach(int item in arr)
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine("\n");
