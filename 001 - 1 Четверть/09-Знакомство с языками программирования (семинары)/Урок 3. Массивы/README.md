d23852b40e7e35a69e0b0f7def253fa3068ce932
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2024-02-26 20:00:11 +0300

    added lesson3 sample1

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 3. Массивы/README.md
@@ -0,0 +1,6 @@
+# Урок 3. Массивы
+
+#### Материалы к курсу:
+* [Запись вебинара](https://gbcdn.mrgcdn.ru/uploads/record/316848/attachment/b7f97c9e991a5647a5044daf81fed599.mp4)
+* [Видеоинструкция по выполнению задач C#](https://gbcdn.mrgcdn.ru/uploads/asset/5752751/attachment/4ce947275c70e68cf97886bd662b8333.mp4)
+* [Презентация](./Презентация.%20Массивы.pdf)
---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 3. Массивы/sample1/Program.cs
@@ -0,0 +1,93 @@
+﻿// Задание 1. Совместная работа
+// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да
+
+
+Console.Write("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] mas = new int[n];
+
+// ---------------------------------
+
+Console.WriteLine(mas);
+
+// ---------------------------------
+
+Console.WriteLine("while method");
+int index = 0;
+while(index < mas.Length) 
+{
+    Console.Write($"{mas[index]} ");
+    index++;
+}
+Console.WriteLine("\n");
+
+Console.WriteLine("for method");
+for(int i = 0; i < mas.Length; i++)
+{
+    Console.Write($"{mas[i]} ");
+}
+Console.WriteLine("\n");
+
+// ---------------------------------
+
+Console.WriteLine("foreach method");
+foreach(var item in mas) 
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine("\n");
+
+// ---------------------------------
+
+Console.WriteLine("void function method");
+
+void printMas(int[] col)
+{
+    foreach(var item in col)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine("\n");
+}
+
+printMas(mas);
+
+// ---------------------------------
+
+Console.WriteLine("Случайные числа");
+
+int[] Random_mas(int[] col)
+{
+    for(int i = 0; i < col.Length; i++)
+    {
+        col[i] = new Random().Next(1,10);
+    }
+    return col;
+}
+
+int[] new_mas = Random_mas(mas);
+
+printMas(new_mas);
+
+// ---------------------------------
+
+Console.WriteLine("Поиск в массиве\n");
+
+Console.Write("Введите число для поиска: ");
+int find = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+for(var i = 0; i < new_mas.Length; i++)
+{
+    if(new_mas[i] == find) 
+    {
+        Console.WriteLine($"Число {find} найден в массиве с индексом {i}");
+        count++;
+    }
+}
+Console.WriteLine($"Искомое число {find}, было найдено {count} раз");
