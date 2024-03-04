bf9300d38d57efc546a34ed3ea490a4014243a1e
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2024-03-04 21:27:38 +0300

    lesson4 task2

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 4. Функции/task2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задание 2. Работа в сессионных залах
+// Задайте массив из N случайных целых чисел (N вводится с
+// клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и
+// делятся нацело на 7.
+// Пример
+// [1 5 11 21 81 4 0 91 2 3]
+// => 2
+
+
+int[] randomArr(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0,100);
+    }
+    return arr;
+}
+
+void printArr(int[] arr)
+{
+    foreach(var item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+int numCount(int[] arr)
+{
+    int count = 0;
+    foreach (var item in arr)
+    {
+        if(item % 10 == 1 && item % 7 == 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = randomArr(new int[n]);
+
+printArr(arr);
+
+Console.WriteLine($"{numCount(arr)}");
