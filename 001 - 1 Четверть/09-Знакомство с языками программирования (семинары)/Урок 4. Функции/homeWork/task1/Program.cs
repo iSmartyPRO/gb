0d2555988003b54dff5d5d37b129db40805f8c06
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2024-03-09 20:02:09 +0300

    update1

--- 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 4. Функции/homeWork/task1/Program.cs
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 4. Функции/homeWork/task1/Program.cs
@@ -6,3 +6,37 @@
 // Пример:
 // 5 12 16 q [STOP]
 // 3 45 342 15 [STOP]
+
+bool checkVar2Quit(string str) {
+    if(checkInt(str)){
+        
+        return false;
+    }
+    else {
+        if(str.Contains('q'))
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
+
+bool checkInt(string str)
+{
+    if(int.TryParse(str, out int value))
+    {
+        return true;
+    }
+    return false;
+}
+
+
+
+
+Console.Write("Введите значение: ");
+string str = Console.ReadLine();
+while(!checkVar2Quit(str)) {
+    Console.Write("Введите значение: ");
+    str = Console.ReadLine();
+}
