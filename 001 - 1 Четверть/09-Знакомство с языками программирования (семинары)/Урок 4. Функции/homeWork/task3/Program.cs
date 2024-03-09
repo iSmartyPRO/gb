7fe29f094af944c05ad9510eea3fb92bac6ad908
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2024-03-09 21:48:58 +0300

    lesson 4 homework task 3

--- 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 4. Функции/homeWork/task3/Program.cs
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 4. Функции/homeWork/task3/Program.cs
@@ -4,3 +4,50 @@
 
 // Пример:
 // [1 3 5 6 7 8] => [8 7 6 5 3 1]
+
+int[] randomArr(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100,1000);
+    }
+    return arr;
+}
+
+void printArr(int[] arr)
+{
+    foreach(var item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+int[] askArrSize() {
+    Console.Write("Введите размерность массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[] arr = randomArr(new int[n]);
+    return arr;
+}
+
+int[] reverseArr(int[] arr)
+{
+    int rNum;
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        rNum = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = rNum;
+    }
+    return arr;
+}
+
+int[] arrSize = askArrSize();
+
+int[] arr = randomArr(arrSize);
+
+Console.WriteLine("Исходный массив: ");
+printArr(arr);
+
+Console.WriteLine("Перевёрнутый массив: ");
+printArr(reverseArr(arr));
