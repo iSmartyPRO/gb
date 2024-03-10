edc4ff0102280d238f4078dbeeb6cba9343fc62a
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2024-03-10 21:02:51 +0300

    lesson 5, tasks and homeworks

---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 5. Двумерные массивы/task1/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задание 1. Совместная работа
+// Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+
+void printArr2(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Squartes(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            {
+                arr[i,j] = arr[i,j] * arr[i,j];
+            }
+        }
+    }
+    return arr;
+}
+
+Console.Write("Введите размерность массива n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите размерность массива m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[n,m];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i,j] = new Random().Next(1, 10);
+    }
+}
+
+Console.WriteLine("Исходный массив:");
+printArr2(arr);
+
+Console.WriteLine();
+Console.WriteLine("Изменённый массив: ");
+printArr2(Squartes(arr));
---
+++ 001 - 1 Четверть/09-Знакомство с языками программирования (семинары)/Урок 5. Двумерные массивы/task2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив. 
+// Найдите сумму элементов, находящихся 
+// на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+
+int[,] InputMatrixArrayFromKeyboard(int n, int m)
+{
+    int[,] arr = new int[n, m];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"Введите {j} элемент для строки {i}: ");
+            arr[i, j] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SummarizeElementsMatrixArray(int[,] arr)
+{
+    int s = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                s = s + arr[i, j];
+            }
+        }
+    }
+    return s;
+}
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = InputMatrixArrayFromKeyboard(n, m);
+PrintArray(arr);
+Console.WriteLine($"Сумма элементов на главной дтиагонали {SummarizeElementsMatrixArray(arr)}");
